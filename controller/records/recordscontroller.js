172b79eaaf95554d3c285494bcd4d18363dd92f0
Saeed Chabok <Saeed.chabok.1994@gmail.com>
2022-02-21 23:42:51 +0100

    Change getidrecords

--- controller/records/recordscontroller.js
+++ controller/records/recordscontroller.js
@@ -48,20 +48,44 @@ const getRecordControllerById = async (req, res) =>
 
 const putRecordControllerById = () =>
 {
-    return(req, res ) => 
+    return async (req, res ) => 
     {
         const { id } = req.params;
-       
-        res.status(200).send('post with ID' + id + 'edited');
+        const controllId = mongoose.isValidObjectId(id);
+        if(!controllId)
+            return res.status(400).send('bad id');
+        const findRecordById = await Records.findById(id);
+        if(!findRecordById)
+            return res.status(404).send('can not found record Id');
+
+        findRecordById.artist= req.body.artist || findRecordById.artist;
+        findRecordById.title= req.body.title || findRecordById.title;
+        findRecordById.year= req.body.year || findRecordById.year;
+        findRecordById.price= req.body.price || findRecordById.price;
+        try 
+        {
+            await findRecordById.save();
+        }
+        catch(error)
+        {
+            console.log(error.message);
+        }    
+        res.status(200).send(findRecordById);
+           
     };
 };
 
 const deleteRecordControllerById = () =>
 {
-    return( req, res) => 
+    return async( req, res) => 
     {
-        const { id } = req.params;
-        res.status(200).send('post with ID' + id + 'delete');
+        const controllId = mongoose.isValidObjectId(req.params.id);
+        if(!controllId)
+            return res.status(400).send('bad id');
+        const deleteRecord = await Records.findByIdAndDelete(req.params.id);
+        if(!deleteRecord)
+            return res.status(404).send('can not found record Id');
+        res.status(200).send(`id ${req.params.id} deleted`);
     };
 };
 module.exports =
