2b41580a1e9f2393e65e1d722ae2f17620de38bf
Saeed Chabok <Saeed.chabok.1994@gmail.com>
2022-03-08 22:42:34 +0100

    Create hashpassword

--- model/userModel.js
+++ model/userModel.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const { userProfileSchema } = require('./userProfile'); 
 const user = new mongoose.Schema(
     {
         firstName: String,
         lastName: String,
         email: String,
-        Passwort: String,
+        Password: String,
         profile: userProfileSchema
     });
+hashPassword = (Password) =>
+{
+    const secret = 'xyz';
+    const hash = crypto.createHmac('sha256', secret).update(Password).digest('hex');
+    return hash;
+};
+
 const userModel = mongoose.model('user', user);
 
 module.exports = userModel;
